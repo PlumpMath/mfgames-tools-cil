7e3427252a20581723800d8da7d1b183ca5b55c4
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-02-20 21:41:07 -0600

    Initial project and an attribute for tools.

---
+++ src/MfGames.Tools/ToolAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MfGames.Tools
+{
+	/// <summary>
+	/// Defines a class that represents a tool in the system. A tool is a generic process
+	/// that can run from the command-line (CLI), a GUI, MSBuild, or another framework.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+    public class ToolAttribute
+    {
+    }
+}
