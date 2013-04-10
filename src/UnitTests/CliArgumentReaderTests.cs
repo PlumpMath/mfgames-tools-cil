0cf9854e107708e0b5e2bbfe1f9227e7cd1a7401
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2013-04-09 20:43:57 -0500

    Work on filling out the unit tests.

--- src/UnitTests/CliArgumentReaderTests.cs
+++ src/UnitTests/CliArgumentReaderTests.cs
@@ -7,6 +7,16 @@ namespace UnitTests
 	[TestFixture]
 	public class CliArgumentReaderTests
 	{
+		/// <summary>
+		/// Creates the CLI argument reader with standard arguments set up.
+		/// 
+		/// <list type="bullet">
+		/// <item>-a, --array: Takes a single value</item>
+		/// <item>-d, --dual: Takes two values</item>
+		/// </list>
+		/// </summary>
+		/// <param name="arguments">The string arguments.</param>
+		/// <returns>A CliArgumentReader with the arguments set up.</returns>
 		private CliArgumentReader CreateCliArgumentReader(string[] arguments)
 		{
 			// Create the initial settings using the Unix-style parameters.
@@ -25,6 +35,19 @@ namespace UnitTests
 					},
 					ValueCount = 1
 				});
+			settings.Arguments.Add(
+				new CliArgument()
+				{
+					LongOptionNames = new List<string>()
+					{
+						"dual"
+					},
+					ShortOptionNames = new List<string>()
+					{
+						"d"
+					},
+					ValueCount = 2
+				});
 
 			// Create the reader and return it.
 			var reader = new CliArgumentReader(settings, arguments);
@@ -34,85 +57,369 @@ namespace UnitTests
 		[Test]
 		public void CountTwoShorts()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"-a", "-a"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"First argument's key was not array.");
+			Assert.IsNull(
+				reader.Values,
+				"First argument's had values.");
+
+			// Act and Assert - Argument 2 (--array=b)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve second argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Second argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"Second argument's key was 'a'.");
+			Assert.IsNull(
+				reader.Values,
+				"Second argument's had values.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseBundledShortWithParam()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"-ab", "param1"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"First argument's had values.");
+
+			// Act and Assert - Argument 2 (-b)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve second argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Second argument's type was not expected.");
+			Assert.AreEqual(
+				"b",
+				reader.Key,
+				"Second argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"Second argument's had values.");
+
+			// Act and Assert - Argument 3 (param1)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve third argument.");
+			Assert.AreEqual(
+				CliArgumentType.Parameter,
+				reader.CliArgumentType,
+				"Third argument's type was not expected.");
+			Assert.AreEqual(
+				"param1",
+				reader.Key,
+				"Third argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"Third argument values was not expected.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseBundledShortWithValue()
 		{
+			// Arrange
 			var arguments = new[]
 			{
-				"-avfilename"
+				"-affilename"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"First argument's had values.");
+
+			// Act and Assert - Argument 2 (-ffilename)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve second argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Second argument's type was not expected.");
+			Assert.AreEqual(
+				"f",
+				reader.Key,
+				"Second argument's key was not expected.");
+			Assert.IsNotNull(
+				reader.Values,
+				"Second argument's did not had values.");
+			Assert.AreEqual(
+				1,
+				reader.Values.Count,
+				"Second's arguments value count was not expected.");
+			Assert.AreEqual(
+				"filename",
+				reader.Values[0],
+				"Second argument's first value was not expected.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseCountedShortBundledWithNonCounted()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"-aab"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"First argument's had values.");
+
+			// Act and Assert - Argument 2 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve second argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Second argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"Second argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"Second argument's had values.");
+
+			// Act and Assert - Argument 3 (param1)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve third argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Third argument's type was not expected.");
+			Assert.AreEqual(
+				"b",
+				reader.Key,
+				"Third argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"Third argument values was not expected.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseCountedShortBundling()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"-aa"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"First argument's had values.");
+
+			// Act and Assert - Argument 2 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve second argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"Second argument's type was not expected.");
+			Assert.AreEqual(
+				"a",
+				reader.Key,
+				"Second argument's key was not expected.");
+			Assert.IsNull(
+				reader.Values,
+				"Second argument's had values.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseDoubleArgumentShort()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"-d", "1", "2"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.ShortOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"d",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNotNull(
+				reader.Values,
+				"First argument's did not had values.");
+			Assert.AreEqual(
+				2,
+				reader.Values.Count,
+				"Second argument's value count was not expected.");
+			Assert.AreEqual(
+				"1",
+				reader.Values[0],
+				"Second argument's first value was not expected.");
+			Assert.AreEqual(
+				"2",
+				reader.Values[1],
+				"Second argument's second value was not expected.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseDoubleValueLong()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"--dual", "1", "2"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
-			reader.Read();
+
+			// Act and Assert - Argument 1 (-a)
+			Assert.IsTrue(
+				reader.Read(),
+				"Could not retrieve first argument.");
+			Assert.AreEqual(
+				CliArgumentType.LongOption,
+				reader.CliArgumentType,
+				"First argument's type was not expected.");
+			Assert.AreEqual(
+				"dual",
+				reader.Key,
+				"First argument's key was not expected.");
+			Assert.IsNotNull(
+				reader.Values,
+				"First argument's did not had values.");
+			Assert.AreEqual(
+				2,
+				reader.Values.Count,
+				"Second argument's value count was not expected.");
+			Assert.AreEqual(
+				"1",
+				reader.Values[0],
+				"Second argument's first value was not expected.");
+			Assert.AreEqual(
+				"2",
+				reader.Values[1],
+				"Second argument's second value was not expected.");
+
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
@@ -122,22 +429,22 @@ namespace UnitTests
 			var arguments = new string[]
 			{
 			};
-
-			// Act
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
 
-			// Assert
-			reader.Read();
+			// Final Act and Assert
+			Assert.IsFalse(
+				reader.Read(),
+				"Retrieved argument when there should be no more.");
 		}
 
 		[Test]
 		public void ParseLongArrayValues()
 		{
+			// Arrange
 			var arguments = new[]
 			{
 				"--array", "a", "--array=b"
 			};
-
 			CliArgumentReader reader = CreateCliArgumentReader(arguments);
 			reader.Read();
 		}
